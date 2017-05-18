b307fb2cbb25edf6d15601ff135b359f6ed5c6fd
yangmei <yangmei@yiran.com>
2017-05-18 20:21:01 +0800

    update

--- createProject.sh
+++ createProject.sh
@@ -3,11 +3,12 @@
 #项目名，首字母大写
 projectName=Demo
 #默认库名
-databaseName=demo
+databaseName='test'
 #小写项目名
 lcProjectName=$(echo $projectName | tr [A-Z] [a-z])
 #基础包名
 package="com.elmman.service.${lcProjectName}"
+packageDir="com/elmman/service/${lcProjectName}"
 #api基础模块
 module_list=('Api' 'Common' 'Core' 'Domain' 'Query' 'Repository')
 
@@ -17,7 +18,7 @@ function createApiProject() {
 
 	createApiModules
 
-	# gradle
+	gradle
 }
 
 
@@ -38,14 +39,14 @@ function createApiModules() {
 		lcModule=$(echo ${module} | tr [A-Z] [a-z])
 		echo "\ninclude '${lcProjectName}-${lcModule}'" >> settings.gradle
 		#创建包
-		mkdir -p ${lcProjectName}-${lcModule}/src/{main,test}/java/${package}.${lcModule}
+		mkdir -p ${lcProjectName}-${lcModule}/src/{main,test}/java/${packageDir}/${lcModule}
 		#创建模板ApplicationContext.java
-		touch ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
-		echo "package ${package}.${lcModule};\n" >> ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
-		echo "\nimport org.springframework.boot.autoconfigure.SpringBootApplication;\n" >> ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
-		echo "@SpringBootApplication" >> ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
-		echo "public class ${projectName}${module}ApplicationContext {\n" >> ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
-		echo "}\n" >> ${lcProjectName}-${lcModule}/src/main/java/${package}.${lcModule}/${projectName}${module}ApplicationContext.java
+		touch ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+		echo "package ${package}.${lcModule};\n" >> ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+		echo "\nimport org.springframework.boot.autoconfigure.SpringBootApplication;\n" >> ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+		echo "@SpringBootApplication" >> ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+		echo "public class ${projectName}${module}ApplicationContext {\n" >> ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+		echo "}\n" >> ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
 		#创建配置目录
 		mkdir -p ${lcProjectName}-${lcModule}/src/main/resources/{base,development,production,testing}
 		#创建base配置文件
@@ -59,7 +60,7 @@ function createApiModules() {
 
 function doSpecialInit() {
 	case $1 in
-		'Api') echo doApiSpecialInit $1 $2
+		'Api') doApiSpecialInit $1 $2
 		;;
 		'Common') echo 'Common'
 		;;
@@ -80,7 +81,11 @@ function doApiSpecialInit() {
 	module=$1
 	lcModule=$2
 
-	
+	mkdir -p ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/{aop,controller}
+
+	sed "s/project_package/${package}/g" ../template/class/ApiApplicationContext.java.example | sed "s/lc_project_name/${lcProjectName}/g" | sed "s/project_name/${projectName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
+
+	sed "s/project_package/${package}/g" ../template/class/HealthController.java.example | sed "s/project_name/${projectName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/controller/HealthController.java
 }
 
 function doRepositorySpecialInit() {
@@ -88,31 +93,28 @@ function doRepositorySpecialInit() {
 	lcModule=$2
 
 	#repository基本目录
-	mkdir -p ${lcProjectName}-${lcModule}/src/main/java/${package}.${module}/{config,datasource,mapper,repository}
+	mkdir -p ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/{config,datasource,mapper,repository}
 
-	sed "s/project_package/${package}/g" ../template/class/DatabaseConfig.java.example | sed "s/project_database/${databaseName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${package}.${module}/config/DatabaseConfig.java
+	sed "s/project_package/${package}/g" ../template/class/DatabaseConfig.java.example | sed "s/project_database/${databaseName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/config/DatabaseConfig.java
 
-	sed "s/project_package/${package}/g" ../template/class/H2DataSource.java.example | sed "s/project_database/${databaseName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${package}.${module}/datasource/H2DataSource.java
+	sed "s/project_package/${package}/g" ../template/class/H2DataSource.java.example | sed "s/project_database/${databaseName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/datasource/H2DataSource.java
 
-	sed "s/project_package/${package}/g" ../template/class/MySQLDataSource.java.example > 	${lcProjectName}-${lcModule}/src/main/java/${package}.${module}/datasource/MySQLDataSource.java
+	sed "s/project_package/${package}/g" ../template/class/MySQLDataSource.java.example > 	${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/datasource/MySQLDataSource.java
 
-	sed "s/project_package/${package}/g" ../template/class/RepositoryApplicationContext.java.example | sed "s/lc_project_name/${lcProjectName}/g" | sed "s/project_name/${projectName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${package}.${module}/${projectName}${module}ApplicationContext.java
+	sed "s/project_package/${package}/g" ../template/class/RepositoryApplicationContext.java.example | sed "s/lc_project_name/${lcProjectName}/g" | sed "s/project_name/${projectName}/g" > ${lcProjectName}-${lcModule}/src/main/java/${packageDir}/${lcModule}/${projectName}${module}ApplicationContext.java
 
 	#base config
 	#生成mybatis generator 配置文件
 	mkdir -p ${lcProjectName}-${lcModule}/src/main/resources/generator
 	sed "s/project_package/${package}/g" ../template/config/generatorConfig.xml.example | sed "s/project_name/${lcProjectName}/g" | sed "s/project_database/${databaseName}/g" > ${lcProjectName}-${lcModule}/src/main/resources/generator/generatorConfig.xml
-	# cp ../template/config/repository.build.gradle.example ${lcProjectName}-${lcModule}/build.gradle
+	sed "s/lc_project_name/${lcProjectName}/g" ../template/config/repository.build.gradle.example > ${lcProjectName}-${lcModule}/build.gradle
 
 	touch ${lcProjectName}-${lcModule}/src/main/resources/H2${databaseName}Schema.sql
 
-	mkdir -p ${lcProjectName}-${lcModule}/src/main/resources/base/${package}.${lcModule}.mapper
+	mkdir -p ${lcProjectName}-${lcModule}/src/main/resources/base/${packageDir}/${lcModule}/mapper
 
 	#development config
-	echo "
-	${databaseName}.jdbc.url=jdbc:mysql://localhost:3306/${databaseName}?zeroDateTimeBehavior=convertToNull&useSSL=false&useUnicode=true&characterEncoding=UTF-8
-	${databaseName}.jdbc.username=root
-	${databaseName}.jdbc.password=123456
+	echo "${databaseName}.jdbc.url=jdbc:mysql://localhost:3306/${databaseName}?zeroDateTimeBehavior=convertToNull&useSSL=false&useUnicode=true&characterEncoding=UTF-8\n${databaseName}.jdbc.username=root\n${databaseName}.jdbc.password=123456
 	" >> ${lcProjectName}-${lcModule}/src/main/resources/development/${lcProjectName}-${lcModule}-env.properties
 }
 
